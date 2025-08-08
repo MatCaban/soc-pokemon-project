34c9c8587fa53d031cd691d24fd28ccaef070363
Matus Caban <153439567+MatCaban@users.noreply.github.com>
2025-08-08 11:31:31 +0200

    delete unused improts

--- src/main/java/com/github/matcaban/pokemonmanager/Main.java
+++ src/main/java/com/github/matcaban/pokemonmanager/Main.java
@@ -2,7 +2,6 @@ package com.github.matcaban.pokemonmanager;
 
 import com.github.matcaban.pokemonmanager.controller.PokemonManager;
 
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
